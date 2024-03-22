79738b944f91909b52b9e313d5bd4b3833a4548b
DCardella2 <156358584+DCardella2@users.noreply.github.com>
2024-03-22 09:26:18 -0700

    Delete plot_frequency.m

--- plot_frequency.m
+++
@@ -1,7 +0,0 @@
-function plot_frequency(audioData)
-    Fs = 44100;
-    stuff = fft(audioData);
-    N = length(audioData);
-    f = (Fs / N) * (-N / 2 : N / 2 - 1);
-    plot(f, abs(fftshift(stuff)));
-end
